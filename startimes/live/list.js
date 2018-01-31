767b8a6a298cb38786413858d9af2938db2b302f
yang.xiaolong <yangxl@startimes.com.cn>
2018-01-31 17:54:51 +0800

    年会直播demo功能完成

---
+++ startimes/live/list.js
@@ -0,0 +1,10 @@
+window.videoList = [
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/daokejiazudenvren/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/kuaiqiangshou/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/xiyouji/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/zanmenxiangaiba/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/huaqiangu/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/gongfushaolin/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/shiyueweicheng/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/zhuoyaoji/143456.m3u8',
+]
