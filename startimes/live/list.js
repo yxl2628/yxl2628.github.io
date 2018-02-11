56ab3e4cfcb883310a453a419bbcf81b49bbf587
yang.xiaolong <yangxl@startimes.com.cn>
2018-02-11 10:27:28 +0800

    修改demo

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,4 +1,6 @@
 window.videoList = [
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangc2_11_541_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangc2_11_541_CODEC_VIDEO_H264/live.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/daokejiazudenvren/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/kuaiqiangshou/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/xiyouji/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/zanmenxiangaiba/143456.m3u8',
 ]
