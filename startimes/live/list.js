1d52eeb126236f8c95fac33990f061a967c73c25
yang.xiaolong <yangxl@startimes.com.cn>
2018-02-11 13:21:35 +0800

    修改样式布局

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -3,4 +3,6 @@ window.videoList = [
   'https://newtranstar.oss-cn-beijing.aliyuncs.com/kuaiqiangshou/143456.m3u8',
   'https://newtranstar.oss-cn-beijing.aliyuncs.com/xiyouji/143456.m3u8',
   'https://newtranstar.oss-cn-beijing.aliyuncs.com/zanmenxiangaiba/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/huaqiangu/143456.m3u8',
+  'https://newtranstar.oss-cn-beijing.aliyuncs.com/gongfushaolin/143456.m3u8',
 ]
