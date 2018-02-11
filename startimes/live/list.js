2de9c2a7b868df3e55656905c84b8cbb2b977164
yang.xiaolong <yangxl@startimes.com.cn>
2018-02-11 08:47:48 +0800

    修改视频

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,3 +1,4 @@
 window.videoList = [
   'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangcc_533_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangccc_535_CODEC_VIDEO_H264/live.m3u8'
 ]
