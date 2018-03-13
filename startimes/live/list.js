3f1a7fe50ba790cfb8c264333acb8e3e4da25e7b
yang.xiaolong <yangxl@startimes.com.cn>
2018-03-13 08:37:35 +0800

    修改路径

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,8 +1,3 @@
 window.videoList = [
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangc2_11_541_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/test002_543_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/test003_555_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/test004_557_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/test005_559_CODEC_VIDEO_H264/live.m3u8',
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/test006_561_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangcteststream4_597_CODEC_VIDEO_H264/live.m3u8',
 ]
