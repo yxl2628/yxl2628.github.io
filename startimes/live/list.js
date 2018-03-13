f8ecb4267cfff1faa03992b805227673d8b1f0ce
yang.xiaolong <yangxl@startimes.com.cn>
2018-03-13 16:32:15 +0800

    电视问诊系统初步完成

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,3 +1,3 @@
 window.videoList = [
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/liangcteststream4_597_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/yey1_601_CODEC_VIDEO_H264/live.m3u8',
 ]
