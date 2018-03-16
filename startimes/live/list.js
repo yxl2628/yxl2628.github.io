73637e0840669339c6e27db76ccec6ad213d0aab
yang.xiaolong <yangxl@startimes.com.cn>
2018-03-16 14:13:01 +0800

    增加域名解析

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,3 +1,4 @@
 window.videoList = [
   'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/yey1_601_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/zhongwei4_613_CODEC_VIDEO_H264/live.m3u8',
 ]
