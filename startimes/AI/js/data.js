16f8439c5ada86cd749984fa52531af62e3934c3
yang.xiaolong <yangxl@startimes.com.cn>
2018-03-22 16:42:21 +0800

    修改图片大小

--- startimes/AI/js/data.js
+++ startimes/AI/js/data.js
@@ -16,7 +16,7 @@ window.zhuangtai = [
   '确定运营商为加纳MTN',
   '数据提取时间：2018.3.21 11:17~2018.3.22 11:18',
   '数据提取中……',
-  '<img src="img/status.png" width="600px" />'
+  '<img src="img/status.png" height="300px" />'
 ]
 window.zihui = [
   '开始自毁倒计时.....','10','9','8','7','6','5','4','3','2','1','haha,haha,haha,我在开玩笑'
