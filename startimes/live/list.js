16c902c8b20bd94f66b29dbaf92da70fdd9d271f
yang.xiaolong <yxl2628@qq.com>
2018-03-29 08:47:25 +0800

    修改视频播放测试地址

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,3 +1,3 @@
 window.videoList = [
-  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/zhongwei4_619_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/zhongweishiji7_627_CODEC_VIDEO_H264/live.m3u8',
 ]
