a577f17da7ad31f7dc7eb4be09a71b9c9ce81f73
yang.xiaolong <yxl2628@qq.com>
2018-03-29 10:38:04 +0800

    修改视频流地址

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,3 +1,4 @@
 window.videoList = [
   'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/zhongweishiji7_627_CODEC_VIDEO_H264/live.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/yey1_621_CODEC_VIDEO_H264/live.m3u8'
 ]
