bce7acf3bb2cd5412aeb757e047209a3bb5f0f5f
yangxl <yangxl@startimes.com.cn>
2018-05-08 10:28:46 +0800

    修改视频地址

--- startimes/live/list.js
+++ startimes/live/list.js
@@ -1,4 +1,4 @@
 window.videoList = [
-  'http://218.205.169.222:8070/live/635.stream/playlist.m3u8',
+  'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/zhongweishiji7_627_CODEC_VIDEO_H264/live.m3u8',
   'http://d37gxy1s0uw2pc.cloudfront.net/html/vod/yey1_621_CODEC_VIDEO_H264/live.m3u8'
 ]
