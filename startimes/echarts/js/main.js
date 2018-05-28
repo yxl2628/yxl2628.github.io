8cdeec68871552822171da6c95f0f731dba38ef8
yangxl <yangxl@startimes.com.cn>
2018-05-28 15:26:57 +0800

    demo完成

--- startimes/echarts/js/main.js
+++ startimes/echarts/js/main.js
@@ -1,13 +1,23 @@
-var zabbix_server = new $.jqzabbix()
-zabbix_server.setOptions({
-  url: 'http://192.168.32.221/api_jsonrpc.php',
-  username: 'search',
-  password: '123456'
+$(document).ready(function(){
+  var zabbix_server = new $.jqzabbix()
+  zabbix_server.setOptions({
+    url: 'http://192.168.32.221/api_jsonrpc.php',
+    username: 'search',
+    password: '123456'
+  })
+  // 先登录获取zabbix的auth
+  zabbix_server.userLogin()
+  // 定时获取各数据
+  // zabbix_server.queryData('item.get',{
+  //   groupids: 36,
+  //   // search: {}
+  // }, function(res) {
+  //   console.log(res)
+  // })
+  // 定时获取汇总数据
+  setInterval(function(){
+    $('#user').html(format_number(213212))
+    $('#network').html(format_number(43241))
+    $('#partner').html(format_number(27))
+  },60000)
 })
-// zabbix_server.userLogin()
-// zabbix_server.queryData('item.get',{
-//   groupids: 36,
-//   // search: {}
-// }, function(res) {
-//   console.log(res)
-// })
