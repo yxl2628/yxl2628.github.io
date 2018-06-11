8ca7f038e35b5fdfeff4be3114e45f346376a281
yangxl <yangxl@startimes.com.cn>
2018-06-11 17:04:40 +0800

    增加monitor滚动效果

--- startimes/monitor/config/monitor-config.js
+++ startimes/monitor/config/monitor-config.js
@@ -3,374 +3,149 @@ var countryList = [
     name: '亚马逊云',
     enName: 'AWS',
     timezone: 'Europe/Dublin',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },{
     name: '埃塞俄比亚',
     enName: 'Ethiopia',
     timezone: 'Africa/Nairobi',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '尼日利亚',
     enName: 'Nigeria',
     timezone: 'Africa/Lagos',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '肯尼亚',
     enName: 'Kenya',
     timezone: 'Africa/Nairobi',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '坦桑',
     enName: 'Tanzania',
     timezone: 'Africa/Nairobi',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '乌干达',
     enName: 'Uganda',
     timezone: 'Africa/Nairobi',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '卢旺达',
     enName: 'Rwanda',
     timezone: 'Africa/Maputo',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '莫桑',
-    enName: 'Mocambique',
+    enName: 'Mozambique',
     timezone: 'Africa/Maputo',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '刚果金',
     enName: 'congo',
     timezone: 'Africa/Lagos',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '几内亚',
     enName: 'Guinea',
     timezone: 'Africa/Abidjan',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '布隆迪',
     enName: 'Burundi',
     timezone: 'Africa/Maputo',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '刚果布',
     enName: 'congo-B',
     timezone: 'Africa/Lagos',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '科特迪瓦',
     enName: 'IvoryCoast',
     timezone: 'Africa/Abidjan',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '加纳',
     enName: 'Ghana',
     timezone: 'Africa/Accra',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '马达',
     enName: 'Madagascar',
     timezone: 'Africa/Nairobi',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '南非',
-    enName: 'South Africa',
+    enName: 'SouthAfrica',
     timezone: 'Africa/Johannesburg',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '利比里亚',
     enName: 'Liberia',
     timezone: 'Africa/Monrovia',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '马拉维',
     enName: 'Malawi',
     timezone: 'Africa/Monrovia',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '赞比亚',
     enName: 'Zambia',
     timezone: 'Africa/Maputo',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '中非',
     enName: 'CentralAfrican',
     timezone: 'Africa/Lagos',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '博茨瓦纳',
     enName: 'Botswana',
     timezone: 'Africa/Maputo',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '喀麦隆',
     enName: 'Cameroon',
     timezone: 'Africa/Lagos',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '塞拉利昂',
     enName: 'SierraLeone',
     timezone: 'Africa/Abidjan',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '莱索托',
     enName: 'Lesotho',
     timezone: 'Africa/Johannesburg',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   },
   {
     name: '马里',
     enName: 'Mali',
     timezone: 'Africa/Abidjan',
-    service: [
-      {
-        name: 'BOSS',
-        status: ''
-      },
-      {
-        name: 'OTT',
-        status: ''
-      }
-    ]
+    hasService: ['BOSS', 'OTT']
   }
 ]
