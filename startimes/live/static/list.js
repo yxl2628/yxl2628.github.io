e1d0b044490f076d2e670cb7cfae7678dc8dfd0f
yangxl <yangxl@startimes.com.cn>
2018-07-25 17:51:42 +0800

    增加肯尼亚视频

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -1,10 +1,27 @@
 window.videoList = [
-  'http://106.38.66.53:8081/video/ke/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke1/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke2/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke3/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke4/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke5/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke6/playlist.m3u8',
-  'http://106.38.66.53:8081/video/ke7/playlist.m3u8',
+  {
+    name: '监控摄像头-肯尼亚1',
+    url: 'http://192.168.32.167:8081/video/ke/playlist.m3u8',
+    low: 'http://192.168.32.167:8081/video/ke/low/playlist.m3u8'
+  },
+  {
+    name: '监控摄像头-肯尼亚2',
+    url: 'http://192.168.32.167:8081/video/ke1/playlist.m3u8',
+    low: 'http://192.168.32.167:8081/video/ke1/low/playlist.m3u8'
+  },
+  {
+    name: '监控摄像头-肯尼亚3',
+    url: 'http://192.168.32.167:8081/video/ke2/playlist.m3u8',
+    low: 'http://192.168.32.167:8081/video/ke2/low/playlist.m3u8'
+  },
+  {
+    name: '监控摄像头-肯尼亚4',
+    url: 'http://192.168.32.167:8081/video/ke3/playlist.m3u8',
+    low: 'http://192.168.32.167:8081/video/ke3/low/playlist.m3u8'
+  },
+  {
+    name: '监控摄像头-肯尼亚5',
+    url: 'http://192.168.32.167:8081/video/ke4/playlist.m3u8',
+    low: 'http://192.168.32.167:8081/video/ke4/low/playlist.m3u8'
+  },
 ]
