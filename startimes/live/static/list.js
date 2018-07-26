d14e026348373aef2784d0de76a81819f6f4c477
yangxl <yangxl@startimes.com.cn>
2018-07-26 18:15:30 +0800

    增加视频调度

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -1,22 +1,22 @@
 window.videoList = [
   {
     name: '监控摄像头-肯尼亚1',
-    url: 'http://192.168.32.167:8081/video/ke2/playlist.m3u8',
+    url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke2/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚2',
-    url: 'http://192.168.32.167:8081/video/ke1/playlist.m3u8',
+    url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke1/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚3',
-    url: 'http://192.168.32.167:8081/video/ke/playlist.m3u8',
+    url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚4',
-    url: 'http://192.168.32.167:8081/video/ke3/playlist.m3u8',
+    url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke3/low/playlist.m3u8'
   },
   // {
---
+++ startimes/live/video.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: '监控摄像头-肯尼亚1',
+    url: 'rtsp://admin:Startimes123@10.12.252.160:554/Streaming/Channels/101?transportmode=unicast'
+  },
+  {
+    name: '监控摄像头-肯尼亚2',
+    url: 'rtsp://admin:Startimes123@10.12.252.161:554/Streaming/Channels/101?transportmode=unicast'
+  },
+  {
+    name: '监控摄像头-肯尼亚3',
+    url: 'rtsp://admin:Startimes123@10.12.252.163:554/Streaming/Channels/101?transportmode=unicast'
+  },
+  {
+    name: '监控摄像头-肯尼亚4',
+    url: 'rtsp://admin:Startimes123@10.12.252.164:554/Streaming/Channels/101?transportmode=unicast'
+  },
+  // {
+  //   name: '监控摄像头-肯尼亚5',
+  //   url: 'http://192.168.32.167:8081/video/ke4/playlist.m3u8'
+  // },
+]
