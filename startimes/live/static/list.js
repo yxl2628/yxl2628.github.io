ab529fb48a3ce0dfe880529b69c7585d27726faf
yangxl <yangxl@startimes.com.cn>
2018-07-27 18:09:30 +0800

    增加了ckplayer的模式

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -1,27 +1,31 @@
 window.videoList = [
   {
     name: '监控摄像头-肯尼亚1',
-    rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
+    rtmp: 'http://10.0.224.107:8080/live/livestream.flv',
     url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚2',
+    rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
     url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke1/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚3',
+    rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
     url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke2/low/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚4',
+    rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
     url: 'http://192.168.32.167:8081/monitor/playlist.m3u8',
     low: 'http://192.168.32.167:8081/video/ke3/low/playlist.m3u8'
   },
   // {
   //   name: '监控摄像头-肯尼亚5',
+  //   rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
   //   url: 'http://192.168.32.167:8081/video/ke4/playlist.m3u8',
   //   low: 'http://192.168.32.167:8081/video/ke4/low/playlist.m3u8'
   // },
