1a19a77d350720dd0cf8cd428478aeeccafe7e89
yangxl <yangxl@startimes.com.cn>
2018-07-31 13:13:33 +0800

    修改为回源地址

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -1,28 +1,28 @@
 window.videoList = [
   {
     name: '监控摄像头-肯尼亚1',
-    rtmp: 'http://192.168.32.167:8084/live/livestream1.flv',
+    rtmp: 'http://192.168.32.167:8081/live/livestream1.flv',
     // url: 'http://192.168.32.167:8084/monitor/playlist.m3u8',
     // low: 'http://192.168.32.167:8084/live/little_livestream11.flv',
     low: 'http://192.168.32.167:8084/video1/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚2',
-    rtmp: 'http://192.168.32.167:8084/live/livestream2.flv',
+    rtmp: 'http://192.168.32.167:8081/live/livestream2.flv',
     // url: 'http://192.168.32.167:8084/monitor/playlist.m3u8',
     // low: 'http://192.168.32.167:8084/live/little_livestream22.flv',
     low: 'http://192.168.32.167:8084/video2/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚3',
-    rtmp: 'http://192.168.32.167:8084/live/livestream3.flv',
+    rtmp: 'http://192.168.32.167:8081/live/livestream3.flv',
     // url: 'http://192.168.32.167:8084/monitor/playlist.m3u8',
     // low: 'http://192.168.32.167:8084/live/little_livestream33.flv',
     low: 'http://192.168.32.167:8084/video3/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚4',
-    rtmp: 'http://192.168.32.167:8084/live/livestream4.flv',
+    rtmp: 'http://192.168.32.167:8081/live/livestream4.flv',
     // url: 'http://192.168.32.167:8084/monitor/playlist.m3u8',
     // low: 'http://192.168.32.167:8084/live/little_livestream44.flv',
     low: 'http://192.168.32.167:8084/video4/playlist.m3u8'
