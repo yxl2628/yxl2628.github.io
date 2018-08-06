9805eac83c21e64ed1f1fbcbe70398a5282157d7
yang.xiaolong <yxl2628@qq.com>
2018-08-06 22:29:22 +0800

    增加国家列表

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -1,30 +1,41 @@
-window.videoList = [
-  {
-    name: '监控摄像头-肯尼亚1',
-    rtmp:  'http://' + window.location.hostname + ':8082/live/livestream1.flv',
-    // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    low:  'http://' + window.location.hostname + ':8082/live/livestream5.flv',
-    // low:  'http://' + window.location.hostname + ':8084/video1/playlist.m3u8'
-  },
-  {
-    name: '监控摄像头-肯尼亚2',
-    rtmp:  'http://' + window.location.hostname + ':8082/live/livestream2.flv',
-    // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    low:  'http://' + window.location.hostname + ':8082/live/livestream6.flv',
-    // low:  'http://' + window.location.hostname + ':8084/video2/playlist.m3u8'
-  },
-  {
-    name: '监控摄像头-肯尼亚3',
-    rtmp:  'http://' + window.location.hostname + ':8082/live/livestream3.flv',
-    // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    low:  'http://' + window.location.hostname + ':8082/live/livestream7.flv',
-    // low:  'http://' + window.location.hostname + ':8084/video3/playlist.m3u8'
-  },
-  {
-    name: '监控摄像头-肯尼亚4',
-    rtmp:  'http://' + window.location.hostname + ':8082/live/livestream4.flv',
-    // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    low:  'http://' + window.location.hostname + ':8082/live/livestream8.flv',
-    // low:  'http://' + window.location.hostname + ':8084/video4/playlist.m3u8'
-  }
-]
+window.videoList = {
+  '1': [
+    {
+      name: '监控摄像头-肯尼亚1',
+      rtmp:  'http://' + window.location.hostname + ':8082/live/livestream1.flv',
+      // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
+      low:  'http://' + window.location.hostname + ':8082/live/livestream5.flv',
+      // low:  'http://' + window.location.hostname + ':8084/video1/playlist.m3u8'
+    },
+    {
+      name: '监控摄像头-肯尼亚2',
+      rtmp:  'http://' + window.location.hostname + ':8082/live/livestream2.flv',
+      // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
+      low:  'http://' + window.location.hostname + ':8082/live/livestream6.flv',
+      // low:  'http://' + window.location.hostname + ':8084/video2/playlist.m3u8'
+    },
+    {
+      name: '监控摄像头-肯尼亚3',
+      rtmp:  'http://' + window.location.hostname + ':8082/live/livestream3.flv',
+      // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
+      low:  'http://' + window.location.hostname + ':8082/live/livestream7.flv',
+      // low:  'http://' + window.location.hostname + ':8084/video3/playlist.m3u8'
+    },
+    {
+      name: '监控摄像头-肯尼亚4',
+      rtmp:  'http://' + window.location.hostname + ':8082/live/livestream4.flv',
+      // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
+      low:  'http://' + window.location.hostname + ':8082/live/livestream8.flv',
+      // low:  'http://' + window.location.hostname + ':8084/video4/playlist.m3u8'
+    }
+  ],
+  '2': [
+    {
+      name: '监控摄像头-莫桑比克1',
+      rtmp:  'http://' + window.location.hostname + ':8083/live/livestream1.flv',
+      // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
+      low:  'http://' + window.location.hostname + ':8083/live/livestream5.flv',
+      // low:  'http://' + window.location.hostname + ':8084/video1/playlist.m3u8'
+    }
+  ]
+}
