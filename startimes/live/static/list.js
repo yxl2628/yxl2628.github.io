a3da6b60518cd6ebc9990b2242caea340e21b5f3
yangxl <yangxl@startimes.com.cn>
2018-08-06 17:58:06 +0800

    修改显示形式，全部改为rtmp

--- startimes/live/static/list.js
+++ startimes/live/static/list.js
@@ -3,34 +3,28 @@ window.videoList = [
     name: '监控摄像头-肯尼亚1',
     rtmp:  'http://' + window.location.hostname + ':8082/live/livestream1.flv',
     // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    // low:  'http://' + window.location.hostname + ':8084/live/little_livestream11.flv',
-    low:  'http://' + window.location.hostname + ':8084/video1/playlist.m3u8'
+    low:  'http://' + window.location.hostname + ':8082/live/livestream5.flv',
+    // low:  'http://' + window.location.hostname + ':8084/video1/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚2',
     rtmp:  'http://' + window.location.hostname + ':8082/live/livestream2.flv',
     // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    // low:  'http://' + window.location.hostname + ':8084/live/little_livestream22.flv',
-    low:  'http://' + window.location.hostname + ':8084/video2/playlist.m3u8'
+    low:  'http://' + window.location.hostname + ':8082/live/livestream6.flv',
+    // low:  'http://' + window.location.hostname + ':8084/video2/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚3',
     rtmp:  'http://' + window.location.hostname + ':8082/live/livestream3.flv',
     // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    // low:  'http://' + window.location.hostname + ':8084/live/little_livestream33.flv',
-    low:  'http://' + window.location.hostname + ':8084/video3/playlist.m3u8'
+    low:  'http://' + window.location.hostname + ':8082/live/livestream7.flv',
+    // low:  'http://' + window.location.hostname + ':8084/video3/playlist.m3u8'
   },
   {
     name: '监控摄像头-肯尼亚4',
     rtmp:  'http://' + window.location.hostname + ':8082/live/livestream4.flv',
     // url:  'http://' + window.location.hostname + ':8084/monitor/playlist.m3u8',
-    // low:  'http://' + window.location.hostname + ':8084/live/little_livestream44.flv',
-    low:  'http://' + window.location.hostname + ':8084/video4/playlist.m3u8'
-  },
-  // {
-  //   name: '监控摄像头-肯尼亚5',
-  //   rtmp: 'rtmp://live.hkstv.hk.lxdns.com/live/hks',
-  //   url:  'http://' + window.location.hostname + ':8084/video/ke4/playlist.m3u8',
-  //   low:  'http://' + window.location.hostname + ':8084/video/ke4/low/playlist.m3u8'
-  // },
+    low:  'http://' + window.location.hostname + ':8082/live/livestream8.flv',
+    // low:  'http://' + window.location.hostname + ':8084/video4/playlist.m3u8'
+  }
 ]
