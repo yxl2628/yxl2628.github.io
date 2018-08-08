58cac5c0b877be08a5529c6316be91e3ae9a44db
yangxl <yangxl@startimes.com.cn>
2018-08-08 10:10:55 +0800

    监控等待联调

--- startimes/live/static/video-list.js
+++ startimes/live/static/video-list.js
@@ -7,21 +7,25 @@ var videoList = {
     list: [
       {
         name: '播控中心-肯尼亚',
+        targetIp: '10.12.252.160',
         rtmp:  'http://' + baseURL + ':8082/live/livestream1.flv',
         low:  'http://' + baseURL + ':8082/live/livestream5.flv',
       },
       {
         name: '监控室-肯尼亚',
+        targetIp: '10.12.252.161',
         rtmp:  'http://' + baseURL + ':8082/live/livestream2.flv',
         low:  'http://' + baseURL + ':8082/live/livestream6.flv',
       },
       {
         name: '户外-肯尼亚',
+        targetIp: '10.12.252.163',
         rtmp:  'http://' + baseURL + ':8082/live/livestream3.flv',
         low:  'http://' + baseURL + ':8082/live/livestream7.flv',
       },
       {
         name: '机房-肯尼亚',
+        targetIp: '10.12.252.164',
         rtmp:  'http://' + baseURL + ':8082/live/livestream4.flv',
         low:  'http://' + baseURL + ':8082/live/livestream8.flv',
       }
@@ -33,6 +37,7 @@ var videoList = {
     list: [
       {
         name: '播控中心-莫桑比克',
+        targetIp: '127.0.0.1',
         rtmp:  'http://' + baseURL + ':8083/live/livestream1.flv',
         low:  'http://' + baseURL + ':8083/live/livestream5.flv',
       }
