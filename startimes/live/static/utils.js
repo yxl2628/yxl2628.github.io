4e5d116dde03ccdae5f60acd6006e489ea679f22
yangxl <yangxl@startimes.com.cn>
2018-08-17 15:12:19 +0800

    改成一个国家一个链接

--- startimes/live/static/utils.js
+++ startimes/live/static/utils.js
@@ -10,9 +10,9 @@ function controlPost(method, commandType, success) {
   var targetIp = currentVideoList[videoIndex].targetIp
   var speed = $('.controler-speed > li.active').attr('data-speed')
   $.ajax({
-    type : "POST",
+    type: "POST",
     url: 'http://' + baseURL + ':8083/monitorControl' + method,
-    data :JSON.stringify({
+    data: JSON.stringify({
       commandType: commandType,
       targetDVR: targetIp,
       speed: speed
@@ -21,4 +21,26 @@ function controlPost(method, commandType, success) {
       success && success(res)
     }
   })
+}
+// 获取星期
+function getWeek() {
+  $('#week').text('星期' + '日一二三四五六'.charAt(new Date().getDay()))
+}
+// 获取日期
+function getDate() {
+  var now = new Date()
+  var month = now.getMonth() + 1
+  var day = now.getDate()
+  month = month < 10 ? '0' + month : month
+  day = day < 10 ? '0' + day : day
+  $('#date').text(month + '.' + day)
+}
+// 获取时间
+function getTime() {
+  var now = new Date()
+  var hour = now.getHours()
+  var minute = now.getMinutes()
+  hour = hour < 10 ? '0' + hour : hour
+  minute = minute < 10 ? '0' + minute : minute
+  $('#time').text(hour + ' : ' + minute)
 }
