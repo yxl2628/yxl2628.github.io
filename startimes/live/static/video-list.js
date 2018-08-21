5e44a422b15ffff0b51d9233ab0daf9689375292
yang.xiaolong <yxl2628@qq.com>
2018-08-21 13:46:40 +0800

    修改地址错误

--- startimes/live/static/video-list.js
+++ startimes/live/static/video-list.js
@@ -2,6 +2,36 @@ var baseURL = window.location.hostname
 baseURL = (baseURL == '127.0.0.1' || baseURL == '192.168.20.61') ? '192.168.32.167' : baseURL
 var videoList = {
   '1': {
+    name: '尼日利亚',
+    enName: 'Nigeria',
+    list: [
+      {
+        name: 'Noc监控',
+        targetIp: '10.4.252.162',
+        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream1.flv',
+        low:  'http://' + baseURL + ':8084/abj_live/livestream4.flv',
+        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
+        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
+      },
+      {
+        name: '数据机房',
+        targetIp: '10.4.252.163',
+        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream2.flv',
+        low:  'http://' + baseURL + ':8084/abj_live/livestream5.flv',
+        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
+        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
+      },
+      {
+        name: '室外发射台',
+        targetIp: '10.4.252.164',
+        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream3.flv',
+        low:  'http://' + baseURL + ':8084/abj_live/livestream6.flv',
+        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
+        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
+      }
+    ]
+  },
+  '2': {
     name: '肯尼亚',
     enName: 'Kenya',
     list: [
@@ -36,36 +66,6 @@ var videoList = {
         low:  'http://' + baseURL + ':8084/ken_live/livestream8.flv',
         rtsp: 'rtsp://' + baseURL + ':5555/proxyStream-4',
         rtspLow: 'rtsp://' + baseURL + ':5555/proxyStream-8'
-      }
-    ]
-  },
-  '2': {
-    name: '尼日利亚',
-    enName: 'Nigeria',
-    list: [
-      {
-        name: 'Noc监控',
-        targetIp: '10.4.252.162',
-        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream1.flv',
-        low:  'http://' + baseURL + ':8084/abj_live/livestream4.flv',
-        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
-        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
-      },
-      {
-        name: '数据机房',
-        targetIp: '10.4.252.163',
-        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream2.flv',
-        low:  'http://' + baseURL + ':8084/abj_live/livestream5.flv',
-        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
-        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
-      },
-      {
-        name: '室外发射台',
-        targetIp: '10.4.252.164',
-        rtmp:  'http://' + baseURL + ':8084/abj_live/livestream3.flv',
-        low:  'http://' + baseURL + ':8084/abj_live/livestream6.flv',
-        rtsp: 'rtsp://' + baseURL + '/proxyStream-1',
-        rtspLow: 'rtsp://' + baseURL + '/proxyStream-1'
       }
     ]
   },
