b933257128ef5cadc11a937410770730029bcbe4
yangxl <327976201@qq.com>
2019-02-15 15:14:12 +0800

    change licence

--- other/licence/wm.js
+++ other/licence/wm.js
@@ -1,2 +1,1 @@
-// var wm_licence = 'U2FsdGVkX19LbMVBL1a58SbHBc+v+qeaYpudPEFtwPsp/XLYt1KcK004Sp4O2UXnLHhgahVYNJrcEXPgZnmttw=='
-var wm_licence = 'U2FsdGVkX19VMBYLz1QrJxWfXG2QBr3fLWu2Yiqsg6Pf3BMzWeEunIT1ijVrAY5D4seKEF3aAIuSONWufRPTLA=='
+var wm_licence = 'U2FsdGVkX19LbMVBL1a58SbHBc+v+qeaYpudPEFtwPsp/XLYt1KcK004Sp4O2UXnLHhgahVYNJrcEXPgZnmttw=='
