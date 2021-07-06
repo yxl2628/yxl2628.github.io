fec40e2cbc43df6a7cb03a22471a2b8ea14abce4
dev135208 <dev135208@qq.com>
2021-07-06 22:43:48 +0800

    feat: 增加点击事件

--- demo/threejs/add.js
+++ demo/threejs/add.js
@@ -2,6 +2,7 @@
 function addFloor() {
   // 添加第一层平面
   addBox({
+    name: '第一层',
     size: [500, 5, 500],
     params: {
       color: 0x6593ce,
@@ -11,6 +12,7 @@ function addFloor() {
   });
   // 添加第二层平面
   addBox({
+    name: '第二层',
     size: [500, 5, 500],
     position: {
       x: 0,
@@ -25,6 +27,7 @@ function addFloor() {
   });
   // 添加第三层平面
   addBox({
+    name: '第三层',
     size: [500, 5, 500],
     position: {
       x: 0,
@@ -42,10 +45,11 @@ function addFloor() {
 function addFirst() {
   // 添加四个路由
   addCylinder({
+    name: '第一个路由',
     size: [40, 40, 20, 40],
     position: {
       x: 100,
-      y: 220,
+      y: 210,
       z: 100
     },
     textture: './images/route.png',
@@ -54,10 +58,11 @@ function addFirst() {
     }
   });
   addCylinder({
+    name: '第二个路由',
     size: [40, 40, 20, 40],
     position: {
       x: -100,
-      y: 220,
+      y: 210,
       z: -100
     },
     textture: './images/route.png',
@@ -66,10 +71,11 @@ function addFirst() {
     }
   });
   addCylinder({
+    name: '第三个路由',
     size: [40, 40, 20, 40],
     position: {
       x: 100,
-      y: 220,
+      y: 210,
       z: -100
     },
     textture: './images/route.png',
@@ -78,10 +84,11 @@ function addFirst() {
     }
   });
   addCylinder({
+    name: '第四个路由',
     size: [40, 40, 20, 40],
     position: {
       x: -100,
-      y: 220,
+      y: 210,
       z: 100
     },
     textture: './images/route.png',
@@ -91,6 +98,7 @@ function addFirst() {
   });
   // 添加一个防火墙
   addBox({
+    name: '防火墙',
     size: [400, 105, 20],
     position: {
       x: 0,
@@ -99,5 +107,33 @@ function addFirst() {
     },
     textture: './images/firewall.jpeg',
     params: {}
+  });
+}
+
+function addSecond() {
+  // 添加一个服务器
+  addBox({
+    name: '第一个服务器',
+    size: [200, 40, 50],
+    position: {
+      x: 0,
+      y: 20,
+      z: 100
+    },
+    params: {
+      color: 0x000000,
+    }
+  });
+  addBox({
+    name: '第二个服务器',
+    size: [200, 40, 50],
+    position: {
+      x: 0,
+      y: 20,
+      z: -100
+    },
+    params: {
+      color: 0x000000,
+    }
   });
 }
--- demo/threejs/main.js
+++ demo/threejs/main.js
@@ -1,18 +1,19 @@
 // 初始化
 function init() {
   /**
-    * 创建场景对象Scene
-    */
+   * 创建场景对象Scene
+   */
   scene = new THREE.Scene();
   /**
-     * 相机设置
-     */
+   * 相机设置
+   */
   width = window.innerWidth; //窗口宽度
   height = window.innerHeight; //窗口高度
   var k = width / height; //窗口宽高比
   var s = 400; //三维场景显示范围控制系数，系数越大，显示的范围越大
   //创建相机对象
   camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+  // camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 2000);
   camera.position.set(276, 137, 274); //设置相机位置
   camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
   // 光源设置
@@ -25,33 +26,34 @@ function init() {
 }
 
 // 渲染
-function render() {
+function initRender() {
   /**
-     * 创建渲染器对象
-     */
-  var renderer = new THREE.WebGLRenderer();
+   * 创建渲染器对象
+   */
+  renderer = new THREE.WebGLRenderer();
   // 设置渲染区域尺寸
   renderer.setSize(width, height);
   // 设置背景颜色
   renderer.setClearColor(0xffffff, 1);
   // body元素中插入canvas对象
   document.body.appendChild(renderer.domElement);
-  function animate() {
-    requestAnimationFrame(animate);
+
+  // 设置鼠标操作
+  controls = new THREE.OrbitControls(camera, renderer.domElement); // 创建控件对象
+  controls.enableZoom = true; // 允许缩放
+  
+  // 渲染
+  function render() {
+    requestAnimationFrame(render);
     renderer.render(scene, camera);
   }
-  animate();
-  // 设置鼠标操作
-  var controls = new THREE.OrbitControls(camera, renderer.domElement); //创建控件对象
-  controls.addEventListener('change', function() {
-    renderer.render(scene, camera);
-  }); //监听鼠标、键盘事件
+  render();
 }
 
 // 添加几何体
 function addBox(options) {
   // 创建长方体 参数：长，宽，高
-  var geometry  = new THREE.BoxGeometry(options.size[0], options.size[1], options.size[2]);
+  var geometry = new THREE.BoxGeometry(options.size[0], options.size[1], options.size[2]);
   if (options.textture) {
     var texture = new THREE.TextureLoader().load(options.textture);
     options.params.map = texture;
@@ -65,6 +67,7 @@ function addBox(options) {
   if (options.position) {
     mesh.position.set(options.position.x, options.position.y, options.position.z);
   }
+  mesh.name = options.name;
   // 网格模型添加到场景中
   scene.add(mesh);
 }
@@ -78,7 +81,9 @@ function addCylinder(options) {
   var material = new THREE.MeshLambertMaterial(options.params);
   if (options.textture) {
     var textture = new THREE.TextureLoader().load(options.textture);
-    texttureMaterial = new THREE.MeshLambertMaterial({ map: textture });
+    texttureMaterial = new THREE.MeshLambertMaterial({
+      map: textture,
+    });
     materials = [];
     materials.push(material);
     materials.push(texttureMaterial);
@@ -89,7 +94,29 @@ function addCylinder(options) {
   if (options.position) {
     mesh.position.set(options.position.x, options.position.y, options.position.z);
   }
+  mesh.name = options.name;
   // 网格模型添加到场景中
   scene.add(mesh);
 }
 
+// 鼠标按键监听
+function mouseDownFuc(e) {
+  let raycaster = new THREE.Raycaster(); //光线投射，用于确定鼠标点击位置
+  let mouse = new THREE.Vector2(); //创建二维平面
+  let intersects = getSelsectOBj(mouse, raycaster, e);
+  if (intersects.length > 0) {
+    console.log(intersects[0].object.name);
+  }
+}
+
+//获取事件操作对象
+function getSelsectOBj(mouse, raycaster, e) {
+  //将html坐标系转化为webgl坐标系，并确定鼠标点击位置
+  mouse.x = e.clientX / renderer.domElement.clientWidth * 2 - 1;
+  mouse.y = -(e.clientY / renderer.domElement.clientHeight * 2) + 1;
+  //以camera为z坐标，确定所点击物体的3D空间位置
+  raycaster.setFromCamera(mouse, camera);
+  //确定所点击位置上的物体数量
+  let intersects = raycaster.intersectObjects(scene.children, true);
+  return intersects;
+}
